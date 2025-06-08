b2554c6a84b2900633349328de8cb6b8a485ad0f
uugana4 <uuganbayr126@icloud.com>
2025-06-08 23:09:16 +0800

    Add files via upload

---
+++ BalancedBrackets.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class BalancedBrackets {
+    public static String isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return "NO";
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return "NO";
+                }
+            }
+        }
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < t; i++) {
+            String s = sc.nextLine();
+            System.out.println(isBalanced(s));
+        }
+        sc.close();
+    }
+}
